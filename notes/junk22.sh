bcc8b2cc54bf6ab0c337f38e349155ea94af0366
Claus Mouse <11484197-senoraraton@users.noreply.gitlab.com>
2023-05-27 21:07:59 -0700

    Updated aliases

--- notes/junk22.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-file="./left_prompt.log"
-
-# Monitor file changes using inotifywait
-while inotifywait -e modify "$file" 2>/dev/null; do
-  changes=$(grep -v 'burden' "$file")
-  echo "$changes"
-done
