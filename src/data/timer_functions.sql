19d6996c12a797956ac974753a3055f4f7ceeafe
Claus Mouse <11484197-senoraraton@users.noreply.gitlab.com>
2023-06-24 00:39:08 -0700

    Finalized timer abstraction, functions are friends

--- src/data/timer_functions.sql
+++ src/data/timer_functions.sql
@@ -1,10 +1,8 @@
-WITH deadletters AS (
-    SELECT MAX(id) as max_id
-    FROM timers
-    WHERE mission_name ~ '^dl' AND xp IS NULL
-) 
-
-UPDATE timers
-SET xp = CASE WHEN id = (SELECT max_id FROM deadletters) THEN :'xp'::integer ELSE 0 END,
-    completed_on = NOW()
-WHERE mission_name ~ '^dl' AND xp is NULL;
+CREATE OR REPLACE FUNCTION update_mission(xp_value integer, mission_completed text) RETURNS void AS $$
+BEGIN
+    UPDATE timers
+    SET xp = CASE WHEN id = (SELECT MAX(id) FROM timers WHERE mission_name LIKE (mission_completed || '%') ) THEN xp_value ELSE 0 END,
+        completed_on = NOW()
+    WHERE mission_name LIKE (mission_completed || '%') AND xp IS NULL;
+END;
+$$ LANGUAGE plpgsql;
