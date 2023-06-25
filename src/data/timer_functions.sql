41bdeb28f1dd324f241621e209b151673ddf02a8
Claus Mouse <11484197-senoraraton@users.noreply.gitlab.com>
2023-06-25 15:42:21 -0700

    Another day, another commit

--- src/data/timer_functions.sql
+++ src/data/timer_functions.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION update_mission(xp_value integer, mission_completed text) RETURNS void AS $$
+CREATE OR REPLACE FUNCTION end_mission(xp_value integer, mission_completed text) RETURNS void AS $$
 BEGIN
     UPDATE timers
     SET xp = CASE WHEN id = (SELECT MAX(id) FROM timers WHERE mission_name LIKE (mission_completed || '%') ) THEN xp_value ELSE 0 END,
@@ -7,3 +7,12 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
+CREATE OR REPLACE FUNCTION check_mission_complete() RETURNS TRIGGER AS $$
+BEGIN
+    INSERT INTO missions_table (column1, column2, column3, column4, column5)
+    SELECT NEW.column1, NEW.column2, NEW.column3, NEW.column4, NEW.column5
+    FROM ingress_table
+    
+
+END;
+$$ LANGUAGE plpgsql;
